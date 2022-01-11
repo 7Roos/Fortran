8d0809bb026aa44c190ce4f048c4a23839d6a6a6
Matheus Roos <matheus.7roos@gmail.com>
2022-01-11 02:23:42 -0300

    Firts arq

--- Ahello_world.f90
+++ Ahello_world.f90
@@ -3,7 +3,7 @@ program hello ! Sempre a primeira instrução, ela serve para identificar o cód
 implicit none ! Caso não seja necessário declarar as variáveis, mesmo assim é recomendável incluir, pelo menos, a instrução "implicit none". Esta instrui o compilador a exigir que todas as variáveis usadas pelo programa tenham o seu tipo EXPLICITAMENTE DEFINIDO.
 
 print *, "hello world"
-end program !Encerramento do programa, é obrigatório
+end program hello!Encerramento do programa, é obrigatório
 
 ! A forma mais simples de um programa em Fortran 90/95 é a seguinte:
 ! PROGRAM <nome_do_programa>
---
+++ Ble_numero.f90
@@ -0,0 +1,9 @@
+program le_valor
+implicit none ! Esta instrução deve ser sempre a segunda linha em um programa
+real :: a ! O usuário deve entrar com os valores das variáveis separando-as por vírgulas.
+print *, "Informe um valor de a:"
+read *, a
+print *, "Valor lido:",a
+end program le_valor
+
+Pág.22
